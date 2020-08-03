8aa754402e057b085dcbf49888f25c3e3fa5ce57
saad <saad-mogren@hotmail.com>
2020-08-03 04:24:59 +0300

    add test
    add tests for get_actors and get_movies

--- projects/capstone/starter/test_app.py
+++ projects/capstone/starter/test_app.py
@@ -3,28 +3,50 @@ import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from flaskr import create_app
-from models import setup_db, Actor, Movie
+from app import create_app
+from models import setup_db,Movie,Actor
+
 
 class CapstoneTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
     def setUp(self):
+        """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "capstone_test"
-        self.database_path = "postgres://{}:{}@{}/{}".format('postgres', 'admin', '5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format(
+            'postgres', 'admin', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
+        # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
+            # create all tables
             self.db.create_all()
-        
-        actor = Actor(name = "test", age = 25, gender = "male")
-        actor.insert()
-        movie = Movie(title = "test", release_date = "2020-10-10")
-        movie.insert()
     def tearDown(self):
+        """Executed after reach test"""
         pass
-    
-    if __name__ == "__main__":
+
+    """
+    TODO
+    Write at least one test for each test for successful
+     operation and for expected errors.
+    """
+
+    def test_get_actors_success(self):
+        res = self.client().get('/actors')
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(body['success'], True)
+        self.assertEqual(len(body['actors']), 1)
+    def test_get_movies_success(self):
+        res = self.client().get('/movies')
+        body = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(body['success'], True)
+        self.assertEqual(len(body['movies']), 1)
+# Make the tests conveniently executable
+if __name__ == "__main__":
     unittest.main()
